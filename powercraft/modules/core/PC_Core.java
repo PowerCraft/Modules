96dd9fb104abcfc4fec0635776bd9a5c4fc75553
zeuslightning125 <Zeusoflightning125@gmail.com>
2014-01-06 20:05:53 -0800

    Initial commit

---
+++ powercraft/modules/core/PC_Core.java
@@ -0,0 +1,24 @@
+package powercraft.modules.core;
+
+import powercraft.api.PowerCraft;
+
+/**
+ * The main class for powercraft core
+ */
+public class PC_Core extends PowerCraft {
+	
+	@Override
+	public void preinit(){
+		// Put pre init stuff here
+	}
+	
+	@Override
+	public void init(){
+		// Put init stuff here
+	}
+	
+	@Override
+	public void postinit(){
+		// Put post init stuff here
+	}
+}
