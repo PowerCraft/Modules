dcaac05ff952a18bf6e2093196fceb2e0d4f4e7c
zcraftler <rennsteak@gmail.com>
2014-03-02 12:04:46 +0100

    working laser rotation

--- powercraft/laser/block/PCla_BlockLaser.java
+++ powercraft/laser/block/PCla_BlockLaser.java
@@ -4,8 +4,12 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import powercraft.api.PC_Direction;
 import powercraft.api.PC_IconRegistry;
 import powercraft.api.PC_Vec3;
@@ -22,10 +26,10 @@ public class PCla_BlockLaser extends PC_BlockTileEntity implements PC_RedstoneCo
 
 	@SideOnly(Side.CLIENT)
 	public static IIcon[] icons = new IIcon[3];
-	
+
 	private final double laserT = 0.4f;
-	private final double minLaserP = 0.5 - 0.5*laserT;
-	private final double maxLaserP = 0.5 + 0.5*laserT;//Used in Rendering
+	private final double minLaserP = 0.5 - 0.5 * laserT;
+	private final double maxLaserP = 0.5 + 0.5 * laserT;// Used in Rendering
 
 	public PCla_BlockLaser() {
 		super(Material.wood);
@@ -36,6 +40,26 @@ public class PCla_BlockLaser extends PC_BlockTileEntity implements PC_RedstoneCo
 	@Override
 	public Class<? extends PC_TileEntity> getTileEntityClass() {
 		return PCla_TileEntityLaser.class;
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack itemInHand) {
+		int l = MathHelper.floor_double((double) (entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+		switch (l) {
+		case 0:
+			l = 2;
+			break;
+		case 1:
+			l = 5;
+			break;
+		case 2:
+			l = 3;
+			break;
+		case 3:
+			l = 4;
+			break;
+		}
+		((PCla_TileEntityLaser) world.getTileEntity(x, y, z)).orientation = PC_Direction.fromSide(l);
 	}
 
 	@Override
@@ -62,39 +86,48 @@ public class PCla_BlockLaser extends PC_BlockTileEntity implements PC_RedstoneCo
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, int modelId,
-			RenderBlocks renderer) {
+	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, int modelId, RenderBlocks renderer) {
 		Tessellator tessellator = Tessellator.instance;
 		tessellator.addTranslation(x, y, z);
 		tessellator.setColorRGBA(255, 255, 255, 255);
-		/*PC_ModelHelper.drawBox(new PC_Vec3(0.8, 0.8, -1), new PC_Vec3(0.2, 0.2, 0), tessellator,
-				PCla_BlockLaser.icons[2]);
-		PC_ModelHelper.drawBox(new PC_Vec3(0.8, 0.8, -2), new PC_Vec3(0.2, 0.2, -1), tessellator,
-				PCla_BlockLaser.icons[2]);
-		PC_ModelHelper.drawBox(new PC_Vec3(0.8, 0.8, -3), new PC_Vec3(0.2, 0.2, -2), tessellator,
-				PCla_BlockLaser.icons[2]);*/
-		PC_ModelHelper.drawBlockAsUsual(this, tessellator, 0);
+		/*
+		 * PC_ModelHelper.drawBox(new PC_Vec3(0.8, 0.8, -1), new PC_Vec3(0.2,
+		 * 0.2, 0), tessellator, PCla_BlockLaser.icons[2]);
+		 * PC_ModelHelper.drawBox(new PC_Vec3(0.8, 0.8, -2), new PC_Vec3(0.2,
+		 * 0.2, -1), tessellator, PCla_BlockLaser.icons[2]);
+		 * PC_ModelHelper.drawBox(new PC_Vec3(0.8, 0.8, -3), new PC_Vec3(0.2,
+		 * 0.2, -2), tessellator, PCla_BlockLaser.icons[2]);
+		 */
+		IIcon[] iconsToDraw = new IIcon[6];
+		for (int i = 0; i < 6; i++) {
+			int index = i;
+			if (index == 3)
+				index = 2;
+			else if (index == 2)
+				index = 3;
+			iconsToDraw[i] = (((PCla_TileEntityLaser) world.getTileEntity(x, y, z)).orientation.equals(PC_Direction
+					.fromSide(index))) ? icons[1] : icons[0];
+		}
+		PC_ModelHelper.drawBlockAsUsual(tessellator, iconsToDraw);
 		PCla_TileEntityLaser tileEntity = (PCla_TileEntityLaser) world.getTileEntity(x, y, z);
 
 		for (PC_Vec3I posToDraw : tileEntity.validLaserPos)
 			switch (tileEntity.orientation) {
 			case EAST:
-				PC_ModelHelper.drawBox(new PC_Vec3(posToDraw.x - x, minLaserP, minLaserP), new PC_Vec3(
-						posToDraw.x - x + 1, maxLaserP, maxLaserP), tessellator, PCla_BlockLaser.icons[2]);
+				PC_ModelHelper.drawBox(new PC_Vec3(posToDraw.x - x, minLaserP, minLaserP), new PC_Vec3(posToDraw.x - x
+						+ 1, maxLaserP, maxLaserP), tessellator, PCla_BlockLaser.icons[2]);
 				break;
 			case NORTH:
-				PC_ModelHelper.drawBox(new PC_Vec3(minLaserP, minLaserP, posToDraw.z - z), new PC_Vec3(
-						maxLaserP, maxLaserP, posToDraw.z - z + 1), tessellator, PCla_BlockLaser.icons[2]);
+				PC_ModelHelper.drawBox(new PC_Vec3(minLaserP, minLaserP, posToDraw.z - z), new PC_Vec3(maxLaserP,
+						maxLaserP, posToDraw.z - z + 1), tessellator, PCla_BlockLaser.icons[2]);
 				break;
 			case SOUTH:
-				PC_ModelHelper.drawBox(new PC_Vec3(minLaserP, minLaserP, posToDraw.z - z), new PC_Vec3(
-						maxLaserP, maxLaserP, posToDraw.z - z + 1), tessellator, PCla_BlockLaser.icons[2]);
-				break;
-			case UNKNOWN:
+				PC_ModelHelper.drawBox(new PC_Vec3(minLaserP, minLaserP, posToDraw.z - z), new PC_Vec3(maxLaserP,
+						maxLaserP, posToDraw.z - z + 1), tessellator, PCla_BlockLaser.icons[2]);
 				break;
 			case WEST:
-				PC_ModelHelper.drawBox(new PC_Vec3(posToDraw.x - x, 0.2, 0.2), new PC_Vec3(
-						posToDraw.x - x + 1, 0.8, 0.8), tessellator, PCla_BlockLaser.icons[2]);
+				PC_ModelHelper.drawBox(new PC_Vec3(posToDraw.x - x, minLaserP, minLaserP), new PC_Vec3(posToDraw.x - x
+						+ 1, maxLaserP, maxLaserP), tessellator, PCla_BlockLaser.icons[2]);
 				break;
 			default:
 				break;
@@ -107,6 +140,7 @@ public class PCla_BlockLaser extends PC_BlockTileEntity implements PC_RedstoneCo
 	@SideOnly(Side.CLIENT)
 	public void renderInventoryBlock(int metadata, int modelId, RenderBlocks renderer) {
 		Tessellator tessellator = Tessellator.instance;
+		tessellator.setTranslation(0, 0, 0);
 		PC_ModelHelper.drawBlockAsUsual(this, tessellator, metadata);
 		// Idk why it doesn't render in Inventorys. Someone has an Idea?
 	}
--- powercraft/laser/tileEntity/PCla_TileEntityLaser.java
+++ powercraft/laser/tileEntity/PCla_TileEntityLaser.java
@@ -1,19 +1,18 @@
 package powercraft.laser.tileEntity;
 
 import java.util.Vector;
-
 import powercraft.api.PC_Direction;
 import powercraft.api.PC_Vec3I;
 import powercraft.api.block.PC_TileEntity;
 
 public class PCla_TileEntityLaser extends PC_TileEntity {
 
-	public Vector<PC_Vec3I> validLaserPos = new Vector<PC_Vec3I>(20);// Please use Java 1.6!!
+	public Vector<PC_Vec3I> validLaserPos = new Vector<PC_Vec3I>(20);
 	public PC_Direction orientation;
 	public int maxLaserLength = 15;
 
 	public PCla_TileEntityLaser() {
-		orientation = PC_Direction.SOUTH;
+		orientation = PC_Direction.NORTH;
 	}
 
 	@Override
@@ -24,28 +23,28 @@ public class PCla_TileEntityLaser extends PC_TileEntity {
 	public void updateBlockList() {
 		validLaserPos.clear();
 		switch (orientation) {
-		case EAST:
+		case WEST:
 			for (int xPos = xCoord + 1; xPos < xCoord + maxLaserLength; xPos++)
 				if (worldObj.getBlock(xPos, yCoord, zCoord).isAir(worldObj, xPos, yCoord, zCoord))
 					validLaserPos.add(new PC_Vec3I(xPos, yCoord, zCoord));
 				else
 					return;
 			break;
-		case NORTH:
+		case SOUTH:
 			for (int zPos = zCoord - 1; zPos > zCoord - maxLaserLength; zPos--)
 				if (worldObj.getBlock(xCoord, yCoord, zPos).isAir(worldObj, xCoord, yCoord, zPos))
 					validLaserPos.add(new PC_Vec3I(xCoord, yCoord, zPos));
 				else
 					return;
 			break;
-		case SOUTH:
+		case NORTH:
 			for (int zPos = zCoord + 1; zPos < zCoord + maxLaserLength; zPos++)
 				if (worldObj.getBlock(xCoord, yCoord, zPos).isAir(worldObj, xCoord, yCoord, zPos))
 					validLaserPos.add(new PC_Vec3I(xCoord, yCoord, zPos));
 				else
 					return;
 			break;
-		case WEST:
+		case EAST:
 			for (int xPos = xCoord - 1; xPos > xCoord - maxLaserLength; xPos--)
 				if (worldObj.getBlock(xPos, yCoord, zCoord).isAir(worldObj, xPos, yCoord, zCoord))
 					validLaserPos.add(new PC_Vec3I(xPos, yCoord, zCoord));
