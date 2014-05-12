263b5d5e7841cc15368f6dff8d1b47092016bc08
XOR19 <nils.h.emmerich@gmail.com>
2014-05-12 20:55:44 +0200

    some changes

--- powercraft/test/gui/PCtm_GuiEnergyConsumer.java
+++ powercraft/test/gui/PCtm_GuiEnergyConsumer.java
@@ -4,12 +4,10 @@ import org.lwjgl.input.Keyboard;
 
 import powercraft.api.PC_Vec2I;
 import powercraft.api.gres.PC_GresComponent;
-import powercraft.api.gres.PC_GresContainer;
 import powercraft.api.gres.PC_GresGuiHandler;
 import powercraft.api.gres.PC_GresListBoxWithoutScroll;
 import powercraft.api.gres.PC_GresListBoxWithoutScroll.ElementClicked;
 import powercraft.api.gres.PC_GresNeedFocusFrame;
-import powercraft.api.gres.PC_GresScrollAreaZoomable;
 import powercraft.api.gres.PC_GresWindow;
 import powercraft.api.gres.PC_IGresGui;
 import powercraft.api.gres.events.PC_GresEvent;
@@ -17,26 +15,25 @@ import powercraft.api.gres.events.PC_GresKeyEvent;
 import powercraft.api.gres.events.PC_IGresEventListener;
 import powercraft.api.gres.layout.PC_GresLayoutVertical;
 import powercraft.api.gres.nodesys.PC_GresNodesysGrid;
+import powercraft.api.gres.nodesys.PC_GresNodesysGridView;
 import powercraft.api.gres.nodesys.PC_GresNodesysHelper;
 
 public class PCtm_GuiEnergyConsumer implements PC_IGresGui, PC_IGresEventListener {
 
-	private PC_GresNodesysGrid grid;
+	private PC_GresNodesysGridView gridView;
 	
 	@Override
 	public void initGui(PC_GresGuiHandler gui) {
 		PC_GresWindow window = new PC_GresWindow("Test");
 		window.setLayout(new PC_GresLayoutVertical());
-		PC_GresScrollAreaZoomable zoomable = new PC_GresScrollAreaZoomable();
-		zoomable.setMinSize(new PC_Vec2I(400, 300));
-		zoomable.setPrefSize(new PC_Vec2I(400, 300));
-		zoomable.setSize(new PC_Vec2I(400, 300));
-		PC_GresContainer c = zoomable.getContainer();
-		c.setLayout(new PC_GresLayoutVertical());
-		this.grid = new PC_GresNodesysGrid();
+		this.gridView = new PC_GresNodesysGridView();
+		this.gridView.setMinSize(new PC_Vec2I(400, 220));
+		this.gridView.setPrefSize(new PC_Vec2I(400, 220));
+		this.gridView.setSize(new PC_Vec2I(400, 220));
+		PC_GresNodesysGrid grid = new PC_GresNodesysGrid();
 		PC_GresNodesysHelper.addNodeToGrid(grid, new PC_Vec2I(), 3);
-		c.add(this.grid);
-		window.add(zoomable);
+		this.gridView.add(grid);
+		window.add(this.gridView);
 		gui.add(window);
 		gui.addEventListener(this);
 	}
@@ -59,8 +56,8 @@ public class PCtm_GuiEnergyConsumer implements PC_IGresGui, PC_IGresEventListene
 		}else if(event instanceof ElementClicked){
 			ElementClicked ec = (ElementClicked)event;
 			if(ec.getElement().nextLayer==null){
-				PC_GresNodesysHelper.addNodeToGrid(grid, new PC_Vec2I(), ec.getElement().id);
-				component.getParent().getParent().takeFocus();
+				PC_GresNodesysHelper.addNodeToGrid((PC_GresNodesysGrid) this.gridView.getChildren().get(0), new PC_Vec2I(), ec.getElement().id);
+				//component.getParent().getParent().takeFocus();
 			}
 		}
 	}
